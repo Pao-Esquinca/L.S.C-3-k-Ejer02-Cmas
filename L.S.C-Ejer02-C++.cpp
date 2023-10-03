a3ead3be202f04b531616aef5df7cf16a733e406
Pao-Esquinca <pl836507@gmail.com>
2023-10-02 22:19:02 -0600

    Agregar archivos de proyecto.

---
+++ L.S.C-Ejer02-C++.cpp
@@ -0,0 +1,29 @@
+// L.S.C-Ejer02-C++.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+const int NUM = 8;
+int main()
+{
+    int nums[NUM];
+    int total = 0;
+    for (int i = 0; i < NUM; i++)
+    {
+        std::cout << "Por favor,introduzca el numero";
+        std::cin >> nums[i];
+        total += nums[i];
+    }
+    std::cout << "El total de numeros es" << total << std::endl;
+    system("pause");
+    return 0;
+}
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
